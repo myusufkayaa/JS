1bb7785bb812ace1b98eec4ec499a3799cf2dce8
myusufkayaa <44977918+myusufkayaa@users.noreply.github.com>
2019-11-28 14:35:06 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE ='JS_Class'
+const FILES = ['/JS/CW1.png', '/JS/CountDown.html', '/JS/C2FConverter.html', '/JS/CW3.png', '/JS/CW%234.html','/JS/CH5/EloquentJS.html','/JS/cw6.html','/JS/CW7/Timing.html']
+
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  })
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  console.log('JS_Class', req.url);
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
